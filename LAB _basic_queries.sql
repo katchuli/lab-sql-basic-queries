21904018e4b2340e0824aa2a902488f4f30b3400
Katherine Chulvi <katherinechulvi.da@gmail.com>
2024-01-23 21:54:39 +0100

    almost

---
+++ LAB _basic_queries.sql
@@ -0,0 +1,18 @@
+SHOW tables;
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+
+SELECT release_year FROM film;
+
+
+
+SELECT title FROM film;
+SELECT name FROM language ;
+SELECT first_name FROM staff;
+
+
+
+
