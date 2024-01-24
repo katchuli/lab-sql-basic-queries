f71e349aa06b5fa17b38299ca9edba029ae9ea74
Katherine Chulvi <katherinechulvi.da@gmail.com>
2024-01-24 09:10:58 +0100

    done with dubts in 5.3, 5.4 and 7.3

--- LAB _basic_queries.sql
+++ LAB _basic_queries.sql
@@ -13,6 +13,15 @@ SELECT title FROM film;
 SELECT name FROM language ;
 SELECT first_name FROM staff;
 
+SELECT COUNT(*) FROM store;
+SELECT COUNT(*) FROM staff AS staff_id;
+SELECT COUNT(last_name) FROM actor;
+SELECT film_id, title, lenght FROM film ORDER BY length DESC LIMIT 10;
 
+SELECT * FROM actor WHERE first_name = 'SCARLETT';
+SELECT * FROM film WHERE title LIKE 'ARMAGEDDON' AND length > 100;
+SELECT COUNT(*) AS num_behind_the_scenes_films
+FROM film
+WHERE special_features LIKE 'Behind the Scenes';
 
 
